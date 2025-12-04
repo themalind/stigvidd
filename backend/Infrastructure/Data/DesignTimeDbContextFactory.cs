0a0fe17df2d7568c62f23e29ba04e1ca0b27616a
Themalind <therese.m.lindberg@gmail.com>
2025-12-04 19:05:34 +0100

    Added migration dbcontext factory

---
+++ backend/Infrastructure/Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace Infrastructure.Data
+{
+    // Används bara när man lägger till nya migreringar via command line "dotnet ef migrations add"
+    // Anledningen är att det kan vara svårt eller jobbig att komma åt appsettings när man gör detta.
+    public class StigViddDbContextFactory : IDesignTimeDbContextFactory<StigViddDbContext>
+    {
+        public StigViddDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<StigViddDbContext>();
+            optionsBuilder.UseSqlServer("");
+
+            return new StigViddDbContext(optionsBuilder.Options);
+        }
+    }
+}
+
+
