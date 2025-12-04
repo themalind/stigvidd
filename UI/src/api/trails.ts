d02e3508805bafe8d70af91989054fdcf5860af7
Themalind <therese.m.lindberg@gmail.com>
2025-12-04 19:24:23 +0100

    Added promise and types to fetch methods

--- UI/src/api/trails.ts
+++ UI/src/api/trails.ts
@@ -1,26 +1,27 @@
-import { Trail } from "@/data/types";
+import { Trail, TrailOverviewViewModel } from "@/data/types";
 
-export async function getTrails() {
-  const response = await fetch("http://10.10.240.153:5265/api/Trail");
-
-  if (!response.ok) {
-    throw new Error(`HTTP error ${response.status}`);
-  }
+export async function getTrails(): Promise<Trail[]> {
   try {
+    const response = await fetch("http://10.10.240.153:5265/api/Trail");
+
+    if (!response.ok) {
+      throw new Error(`HTTP error ${response.status}`);
+    }
+
     return response.json();
   } catch {
     throw new Error("Invalid JSON response from server");
   }
 }
 
-export async function getPopularTrails() {
-  const response = await fetch("http://10.10.240.153:5265/api/Trail/popular");
-
-  if (!response.ok) {
-    throw new Error(`HTTP error ${response.status}`);
-  }
-
+export async function getPopularTrails(): Promise<TrailOverviewViewModel[]> {
   try {
+    const response = await fetch("http://10.10.240.153:5265/api/Trail/popular");
+
+    if (!response.ok) {
+      throw new Error(`HTTP error ${response.status}`);
+    }
+
     return response.json();
   } catch {
     throw new Error("Invalid JSON response from server");
@@ -30,13 +31,15 @@ export async function getPopularTrails() {
 export async function fetchTrailByIdentifier(
   identifier: string,
 ): Promise<Trail> {
-  const response = await fetch(
-    `http://10.10.240.153:5265/api/Trail/${identifier}`,
-  );
-  if (!response.ok) {
-    throw new Error(`HTTP error ${response.status}`);
-  }
   try {
+    const response = await fetch(
+      `http://10.10.240.153:5265/api/Trail/${identifier}`,
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error ${response.status}`);
+    }
+
     return await response.json();
   } catch {
     throw new Error("Invalid JSON response from server");
