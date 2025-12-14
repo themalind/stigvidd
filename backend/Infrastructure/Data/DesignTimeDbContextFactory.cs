cb96c4f83e1e9f55bf51a37cb92f986c89edcbd0
Themalind <therese.m.lindberg@gmail.com>
2025-12-14 19:33:05 +0100

    Tried to fix so i wont push connectionstring by misstake again...

---
+++ backend/Infrastructure/Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+
+namespace Infrastructure.Data
+{
+    // Används bara när man lägger till nya migreringar via command line "dotnet ef migrations add"
+    // Anledningen är att det kan vara svårt eller jobbig att komma åt appsettings när man gör detta.
+    public class StigViddDbContextFactory : IDesignTimeDbContextFactory<StigViddDbContext>
+    {
+        public StigViddDbContext CreateDbContext(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .AddUserSecrets<StigViddDbContextFactory>()
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<StigViddDbContext>();
+            optionsBuilder.UseSqlServer(config.GetConnectionString("StigVidd"));
+
+            return new StigViddDbContext(optionsBuilder.Options);
+        }
+    }
+}
+
+
