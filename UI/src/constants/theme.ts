f3d7aa2ad06996dec8901952c5409f1dd9653f0f
Martin B <martinjborjesson@gmail.com>
2026-01-07 15:30:29 +0100

    add darkMapTheme for dark mode

--- UI/src/constants/theme.ts
+++ UI/src/constants/theme.ts
@@ -106,3 +106,20 @@ export const AppDarkTheme = merge(CombinedDarkTheme, {
     backdrop: "rgba(44, 50, 42, 0.4)",
   },
 });
+
+export const darkMapTheme =
+  [
+    { "elementType": "geometry", "stylers": [{ "color": "#212121" }] },
+    { "elementType": "labels.icon", "stylers": [{ "visibility": "off" }] },
+    { "elementType": "labels.text.fill", "stylers": [{ "color": "#757575" }] },
+    { "elementType": "labels.text.stroke", "stylers": [{ "color": "#212121" }] },
+    { "featureType": "administrative", "elementType": "geometry", "stylers": [{ "color": "#757575" }] },
+    { "featureType": "poi", "elementType": "geometry", "stylers": [{ "color": "#303030" }] },
+    { "featureType": "poi.park", "elementType": "geometry", "stylers": [{ "color": "#181818" }] },
+    { "featureType": "road", "elementType": "geometry.fill", "stylers": [{ "color": "#2c2c2c" }] },
+    { "featureType": "road", "elementType": "geometry.stroke", "stylers": [{ "color": "#1c1c1c" }] },
+    { "featureType": "road.highway", "elementType": "geometry.fill", "stylers": [{ "color": "#3c3c3c" }] },
+    { "featureType": "transit", "elementType": "geometry", "stylers": [{ "color": "#2f2f2f" }] },
+    { "featureType": "water", "elementType": "geometry", "stylers": [{ "color": "#000000" }] },
+    { "featureType": "water", "elementType": "labels.text.fill", "stylers": [{ "color": "#3d3d3d" }] }
+  ]
