5a3433df6870d08af146cb5815992b50395a3740
Themalind <therese.m.lindberg@gmail.com>
2026-01-23 18:44:02 +0100

    Response factory

---
+++ backend/Core/Factories/ReviewResponseFactory.cs.cs
@@ -0,0 +1,24 @@
+﻿using Infrastructure.Data.Entities;
+using WebDataContracts.ResponseModels.Review;
+
+namespace Core.Factories;
+
+public class ReviewResponseFactory
+{
+    public ReviewResponse Create(Review review)
+    {
+        var images = review.ReviewImages?.Select(ri =>
+            ReviewImageResponse.Create(
+                ri.Identifier,
+                ri.ImageUrl)) ?? null;
+        return ReviewResponse.Create(
+            review.Identifier,
+            review.TrailReview ?? string.Empty,
+            review.Grade,
+            review.User!.NickName,
+            review.CreatedAt,
+            review.Trail!.Identifier,
+            review.User.Identifier,
+            images);
+    }
+}
