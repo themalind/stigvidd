90aec66d6c938f591399e4a4949e1a0a372d20cd
Themalind <therese.m.lindberg@gmail.com>
2026-01-23 19:00:53 +0100

    add portal to snackbar

--- UI/src/components/global-snackbar.tsx
+++ UI/src/components/global-snackbar.tsx
@@ -5,37 +5,40 @@ import { StyleSheet, View } from "react-native";
 import { Snackbar, Text, useTheme } from "react-native-paper";
 import { hideSnackbarAtom, snackbarAtom } from "../atoms/snackbar-atoms";
 
+import { Portal } from "react-native-paper";
+
 export function GlobalSnackbar() {
   const theme = useTheme();
   const [snackbar] = useAtom(snackbarAtom);
   const hideSnackbar = useSetAtom(hideSnackbarAtom);
 
   return (
-    <Snackbar
-      visible={snackbar.visible}
-      onDismiss={hideSnackbar}
-      duration={3000}
-      wrapperStyle={{ bottom: 50 }}
-      style={{
-        backgroundColor: theme.colors.secondary,
-      }}
-    >
-      <View style={s.contentContainer}>
-        {snackbar.icon && (
-          <MaterialIcons
-            name={snackbar.icon as any}
-            size={20}
-            color={theme.colors.onSecondary}
-          />
-        )}
-        <Text style={{ color: theme.colors.onSecondary }}>
-          {snackbar.message}
-        </Text>
-      </View>
-    </Snackbar>
+    <Portal>
+      <Snackbar
+        visible={snackbar.visible}
+        onDismiss={hideSnackbar}
+        duration={3000}
+        wrapperStyle={{ bottom: 50 }}
+        style={{
+          backgroundColor: theme.colors.secondary,
+        }}
+      >
+        <View style={s.contentContainer}>
+          {snackbar.icon && (
+            <MaterialIcons
+              name={snackbar.icon as any}
+              size={20}
+              color={theme.colors.onSecondary}
+            />
+          )}
+          <Text style={{ color: theme.colors.onSecondary }}>
+            {snackbar.message}
+          </Text>
+        </View>
+      </Snackbar>
+    </Portal>
   );
 }
-
 const s = StyleSheet.create({
   contentContainer: {
     flexDirection: "row",
