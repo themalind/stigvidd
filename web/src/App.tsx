d71972ceafe0a776a90e8befa72b3a32251529ad
Martin B <martinjborjesson@gmail.com>
2026-01-27 17:08:01 +0100

    install vite, shadcn + tailwind and some initial configuring

---
+++ web/src/App.tsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <div className="h-full flex flex-col justify-center items-center gap-4 bg-stone-900">
+      <h1 className="text-4xl text-stone-100">
+        Stigvidd
+      </h1>
+      <p className="text-stone-400">¨
+        Therese skriv något här...
+      </p>
+    </div>
+  )
+}
