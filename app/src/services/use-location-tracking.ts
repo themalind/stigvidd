8c4bca014bc778308ad3dfec53c39ab96c52af0b
Martin B <martinjborjesson@gmail.com>
2026-01-29 17:28:17 +0100

    add hook useLocationTracking

---
+++ app/src/services/use-location-tracking.ts
@@ -0,0 +1,70 @@
+import { useRef, useState } from "react";
+import * as Location from "expo-location";
+import { LatLng } from "react-native-maps";
+import { useSetAtom } from "jotai";
+import { showErrorAtom } from "@/atoms/snackbar-atoms";
+
+const SAMPLE_INTERVAL = 3000;
+
+type LocationData = {
+  coordinates: LatLng;
+  timeStamp: number;
+};
+
+export function useLocationTracking() {
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
+
+  const setError = useSetAtom(showErrorAtom);
+  const [coordinates, setCoordinates] = useState<LocationData[]>([]);
+  const [isTracking, setIsTracking] = useState(false);
+
+  const startTracking = async () => {
+    const { status: fgPermission } = await Location.requestForegroundPermissionsAsync();
+    if (fgPermission !== "granted") {
+      setError("Permission to access location was denied.");
+      return;
+    }
+    const { status: bgPermission } = await Location.requestBackgroundPermissionsAsync();
+    if (bgPermission !== "granted") {
+      setError("Permission to access backgroundLocation was denied.");
+      return;
+    }
+
+    intervalRef.current = setInterval(async () => {
+      try {
+        const location = await Location.getCurrentPositionAsync();
+
+        setCoordinates((prev) => [
+          ...prev,
+          {
+            coordinates: {
+              latitude: location.coords.latitude,
+              longitude: location.coords.longitude,
+            },
+            timeStamp: location.timestamp,
+          },
+        ]);
+      } catch {
+        stopTracking();
+        setError("Lost access to location.");
+        console.log("crash");
+      }
+    }, SAMPLE_INTERVAL);
+
+    setIsTracking(true);
+  };
+
+  const stopTracking = () => {
+    if (intervalRef.current) {
+      clearInterval(intervalRef.current);
+      intervalRef.current = null;
+    }
+    setIsTracking(false);
+  };
+
+  const resetTracking = () => {
+    setCoordinates([]);
+  };
+
+  return { startTracking, stopTracking, resetTracking, isTracking, coordinates };
+}
