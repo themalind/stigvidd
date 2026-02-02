0ec24e44d93877bc2fa111d48a25807c00c91611
Martin B <martinjborjesson@gmail.com>
2026-02-02 14:13:46 +0100

    changes to properties

--- backend/Infrastructure/Data/Entities/Trail.cs
+++ backend/Infrastructure/Data/Entities/Trail.cs
@@ -3,17 +3,18 @@
 public class Trail : BaseEntity
 {
     public required string Name { get; set; }
-    public required double TrailLength { get; set; }
-    public string Classification { get; set; } = string.Empty;
-    public bool Accessability { get; set; } = false;
-    public string AccessabilityInfo { get; set; } = string.Empty;
+    public required decimal TrailLength { get; set; }
+    public int Classification { get; set; } = 0;
+    public bool Accessibility { get; set; } = false;
+    public string AccessibilityInfo { get; set; } = string.Empty;
     public string TrailSymbol { get; set; } = string.Empty;
     public string TrailSymbolImage { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
-    public string? CoordinatesJson { get; set; }
+    public string FullDescription { get; set; } = string.Empty;
+    public string? Coordinates { get; set; }
+    public string? CreatedBy { get; set; }
 
     public ICollection<TrailImage>? TrailImages { get; set; }
     public ICollection<Review>? Reviews { get; set; }
     public ICollection<TrailLink>? TrailLinks { get; set; }
 }
-
