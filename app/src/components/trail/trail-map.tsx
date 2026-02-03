805b76c8ac301513f95ddd700fe6a2cf49fc16db
Martin B <martinjborjesson@gmail.com>
2026-02-03 12:25:24 +0100

    replace mocked trail with real coordinates

--- app/src/components/trail/trail-map.tsx
+++ app/src/components/trail/trail-map.tsx
@@ -1,4 +1,3 @@
-import { Trail } from "@/data/types";
 import { Dimensions, StyleSheet } from "react-native";
 import Map from "../map/map";
 import { Surface } from "react-native-paper";
@@ -6,7 +5,7 @@ import MapView, { LatLng, Polyline } from "react-native-maps";
 import { useEffect, useRef } from "react";
 
 interface TrailMapProps {
-  trail: Trail;
+  trail: LatLng[];
 }
 
 const WIDTH = Dimensions.get("screen").width;
@@ -16,12 +15,10 @@ export default function TrailMap({ trail }: TrailMapProps) {
   const mapRef = useRef<MapView>(null);
   const initialPadding = 60;
 
-  const trailCoordinates: LatLng[] = mock;
-
   useEffect(() => {
-    if (!mapRef.current || trailCoordinates.length === 0) return;
+    if (!mapRef.current || trail.length === 0) return;
 
-    mapRef.current.fitToCoordinates(trailCoordinates, {
+    mapRef.current.fitToCoordinates(trail, {
       edgePadding: {
         top: initialPadding,
         right: initialPadding,
@@ -30,16 +27,12 @@ export default function TrailMap({ trail }: TrailMapProps) {
       },
       animated: true,
     });
-  }, [trailCoordinates]);
+  }, [trail]);
 
   return (
     <Surface style={s.container}>
       <Map style={s.map} ref={mapRef}>
-        <Polyline
-          coordinates={trailCoordinates}
-          strokeWidth={3}
-          strokeColor="#eb3204"
-        />
+        <Polyline coordinates={trail} strokeWidth={3} strokeColor="#eb3204" />
       </Map>
     </Surface>
   );
@@ -56,57 +49,3 @@ const s = StyleSheet.create({
     flex: 1,
   },
 });
-
-const mock: LatLng[] = [
-  { latitude: 57.73, longitude: 12.703 },
-  { latitude: 57.73, longitude: 12.702 },
-  { latitude: 57.731, longitude: 12.702 },
-  { latitude: 57.731, longitude: 12.701 },
-  { latitude: 57.73, longitude: 12.701 },
-  { latitude: 57.73, longitude: 12.7 },
-  { latitude: 57.7325, longitude: 12.7 },
-  { latitude: 57.7325, longitude: 12.701 },
-  { latitude: 57.7315, longitude: 12.701 },
-  { latitude: 57.7315, longitude: 12.702 },
-  { latitude: 57.7325, longitude: 12.702 },
-  { latitude: 57.7325, longitude: 12.703 },
-  { latitude: 57.73, longitude: 12.703 },
-
-  { latitude: 57.73, longitude: 12.7045 },
-
-  { latitude: 57.7325, longitude: 12.7045 },
-  { latitude: 57.7325, longitude: 12.7075 },
-  { latitude: 57.732, longitude: 12.7075 },
-  { latitude: 57.732, longitude: 12.7055 },
-  { latitude: 57.7315, longitude: 12.7055 },
-  { latitude: 57.7315, longitude: 12.7075 },
-  { latitude: 57.731, longitude: 12.7075 },
-  { latitude: 57.731, longitude: 12.7055 },
-  { latitude: 57.7305, longitude: 12.7055 },
-  { latitude: 57.7305, longitude: 12.7075 },
-  { latitude: 57.73, longitude: 12.7075 },
-  { latitude: 57.73, longitude: 12.7045 },
-  { latitude: 57.73, longitude: 12.7075 },
-
-  { latitude: 57.73, longitude: 12.709 },
-
-  { latitude: 57.7325, longitude: 12.709 },
-  { latitude: 57.7325, longitude: 12.71 },
-  { latitude: 57.7305, longitude: 12.71 },
-  { latitude: 57.7305, longitude: 12.712 },
-  { latitude: 57.73, longitude: 12.712 },
-  { latitude: 57.73, longitude: 12.709 },
-
-  { latitude: 57.73, longitude: 12.7135 },
-
-  { latitude: 57.7325, longitude: 12.7135 },
-  { latitude: 57.7325, longitude: 12.7165 },
-  { latitude: 57.731, longitude: 12.7165 },
-  { latitude: 57.731, longitude: 12.7145 },
-  { latitude: 57.732, longitude: 12.7145 },
-  { latitude: 57.732, longitude: 12.7155 },
-  { latitude: 57.7315, longitude: 12.7155 },
-  { latitude: 57.7315, longitude: 12.7145 },
-  { latitude: 57.73, longitude: 12.7145 },
-  { latitude: 57.73, longitude: 12.7135 },
-];
