0c2ca8a1fcad02b18dcb569471afb99a0f42d271
Martin B <martinjborjesson@gmail.com>
2026-02-09 09:56:09 +0100

    add rules to trail valodator

--- backend/Core/Validators/CreateTrailRequestValidator.cs
+++ backend/Core/Validators/CreateTrailRequestValidator.cs
@@ -13,7 +13,21 @@ public class CreateTrailRequestValidator : AbstractValidator<CreateTrailRequest>
         RuleFor(CreateTrailRequest => CreateTrailRequest.TrailLength)
             .GreaterThan(0)
             .LessThan(double.MaxValue);
-        
-        // wip
+        RuleFor(CreateTrailRequest => CreateTrailRequest.Classification)
+            .NotEmpty().WithMessage("Must contain a classification")
+            .GreaterThan(0)
+            .LessThan(4);
+        RuleFor(CreateTrailRequest => CreateTrailRequest.AccessibilityInfo)
+            .MaximumLength(1024);
+        RuleFor(CreateTrailRequest => CreateTrailRequest.TrailSymbol)
+            .MaximumLength(32);
+        RuleFor(CreateTrailRequest => CreateTrailRequest.Description)
+            .NotEmpty().WithMessage("Short description is required")
+            .MaximumLength(256);
+        RuleFor(CreateTrailRequest => CreateTrailRequest.FullDescription)
+            .MaximumLength(1024);
+        RuleFor(CreateTrailRequest => CreateTrailRequest.City)
+            .NotEmpty().WithMessage("City is requierd")
+            .MaximumLength(128);
     }
 }
