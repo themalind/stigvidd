bed2e89d14c60ee4c6fc24ab7e3396ff29307260
Martin B <martinjborjesson@gmail.com>
2026-02-09 09:55:37 +0100

    update CreateTrailRequest to match entity class

--- backend/WebDataContracts/RequestModels/Trail/CreateTrailRequest.cs
+++ backend/WebDataContracts/RequestModels/Trail/CreateTrailRequest.cs
@@ -4,11 +4,14 @@ public class CreateTrailRequest
 {
     public required string Name { get; set; }
     public required double TrailLength { get; set; }
-    public string? Classification { get; set; }
-    public bool? Accessability { get; set; }
-    public string? AccessabilityInfo { get; set; }
+    public int? Classification { get; set; }
+    public bool? Accessibility { get; set; }
+    public string? AccessibilityInfo { get; set; }
     public string? TrailSymbol { get; set; }
     public string? TrailSymbolImage { get; set; }
     public string? Description { get; set; }
+    public string? FullDescription { get; set; }
     public required string Coordinates { get; set; }
+    public string? City { get; set; }
+    public string? CreatedBy { get; set; }
 }
