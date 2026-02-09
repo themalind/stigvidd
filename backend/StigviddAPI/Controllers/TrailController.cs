5cfef2350d6ac528fbbd1711b21abe883b0b9707
Martin B <martinjborjesson@gmail.com>
2026-02-09 11:20:41 +0100

    add endpoint for adding a trail

--- backend/StigviddAPI/Controllers/TrailController.cs
+++ backend/StigviddAPI/Controllers/TrailController.cs
@@ -1,20 +1,30 @@
 ﻿using Core.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using WebDataContracts.RequestModels.Trail;
 using WebDataContracts.ResponseModels.Trail;
 
 namespace StigviddAPI.Controllers;
 
 [ApiController]
 [Route("api/v1/[controller]")]
-public class TrailController(ITrailService service, ILogger<TrailController> logger) : StigViddController
+public class TrailController : StigViddController
 {
-    private readonly ITrailService _service = service;
-    private readonly ILogger<TrailController> _logger = logger;
+    private readonly ITrailService _trailService;
+    private readonly IUserService _userService;
+    private readonly ILogger<TrailController> _logger;
+
+    public TrailController(ITrailService trailService, IUserService userService, Logger<TrailController> logger)
+    {
+        _trailService = trailService;
+        _userService = userService;
+        _logger = logger;
+    }
 
     [Route("{identifier}")]
     public async Task<ActionResult<TrailResponse?>> GetTrailByIdentifierAsync(string identifier, CancellationToken ctoken)
     {
-        var result = await _service.GetTrailByIdentifierAsync(identifier, ctoken);
+        var result = await _trailService.GetTrailByIdentifierAsync(identifier, ctoken);
 
         if (!result.Success && result.Message != null)
         {
@@ -31,7 +41,7 @@ public class TrailController(ITrailService service, ILogger<TrailController> log
     [Route("popular")]
     public async Task<ActionResult<IReadOnlyCollection<TrailOverviewResponse?>>> GetPopularTrailsAsync(CancellationToken ctoken)
     {
-        var result = await _service.GetPopularTrailOverviewsAsync(ctoken);
+        var result = await _trailService.GetPopularTrailOverviewsAsync(ctoken);
 
         if (!result.Success && result.Message != null)
         {
@@ -42,6 +52,37 @@ public class TrailController(ITrailService service, ILogger<TrailController> log
         }
 
         return Ok(result.Value);
+    }
+
+    [Authorize]
+    [HttpPost]
+    [Route("create")]
+    public async Task<ActionResult> AddTrailAsync(
+        [FromForm] CreateTrailRequest request,
+        [FromForm] IFormFile trailSymbolImage,
+        [FromForm] IFormFileCollection images,
+        CancellationToken ctoken)
+    {
+        var userResponse = await GetAuthenticatedUserAsync(_userService, ctoken);
+
+        if (userResponse == null)
+        {
+            return Unauthorized("User not found");
+        }
+
+        var result = await _trailService.AddTrailAsync(request, trailSymbolImage, images, ctoken);
+
+        if (!result.Success && result.Message != null)
+        {
+            return ToActionResult(result.Message);
+        }
+
+        if (result.Value == null)
+        {
+            return BadRequest("Trail could not be created.");
+        }
+
+        return Created($"/api/v1/trail/{result.Value.Identifier}", result.Value);
     }
 }
 
