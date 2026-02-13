7f85815f50162338b92f911cf0316e82575009e4
Martin B <martinjborjesson@gmail.com>
2026-02-13 15:50:03 +0100

    fix theme-provider and add toggle-button for dark mode

---
+++ web/src/components/dark-mode-toggle.tsx
@@ -0,0 +1,21 @@
+import { Moon, Sun } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import useTheme from "@/hooks/use-theme";
+
+export function DarkModeToggle() {
+  const { theme, setTheme } = useTheme();
+
+  const toggleTheme = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  };
+
+  return (
+    <Button variant="ghost" size="icon" onClick={toggleTheme}>
+      {theme === "dark" ? (
+        <Sun className="h-[1.2rem] w-[1.2rem]" />
+      ) : (
+        <Moon className="h-[1.2rem] w-[1.2rem]" />
+      )}
+    </Button>
+  );
+}
