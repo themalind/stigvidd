4c0908492d1b22e3c1a8ed3d364f4e98b9c5e0ab
Martin B <martinjborjesson@gmail.com>
2026-02-19 15:15:02 +0100

    add modal to hold form for saving hike

---
+++ app/src/components/trail/trail-creator/save-hike-modal.tsx
@@ -0,0 +1,36 @@
+import { StyleSheet } from "react-native";
+import { Dialog, Portal, useTheme } from "react-native-paper";
+import SaveHikeForm from "./save-hike-form";
+import { Hike } from "@/data/types";
+
+interface Props {
+  visible: boolean;
+  onDismiss: () => void;
+  onConfirm: () => void;
+  hike: Hike;
+}
+
+export default function SaveHikeModal({ visible, onDismiss, onConfirm, hike }: Props) {
+  const theme = useTheme();
+
+  return (
+    <Portal>
+      <Dialog
+        style={[s.container, { backgroundColor: theme.colors.background }]}
+        visible={visible}
+        onDismiss={onDismiss}
+      >
+        <Dialog.Title>Spara Promenad</Dialog.Title>
+        <Dialog.Content>
+          <SaveHikeForm hike={hike} onDismiss={onDismiss} />
+        </Dialog.Content>
+      </Dialog>
+    </Portal>
+  );
+}
+
+const s = StyleSheet.create({
+  container: {
+    borderRadius: 20,
+  },
+});
