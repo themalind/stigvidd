fa7f5cee027a84dbab5f32adc26172437fd878b5
Martin B <martinjborjesson@gmail.com>
2026-02-20 16:08:03 +0100

    add all apis

--- app/src/api/hikes.ts
+++ app/src/api/hikes.ts
@@ -1,15 +1,121 @@
-export async function createHike() {
-  return null
+import { CreateHikeRequest, Hike } from "@/data/types";
+import { IP } from "../../ipconfig";
+import { ApiError, getUserToken } from "./users";
+
+export async function createHike(request: CreateHikeRequest): Promise<{ success: boolean }> {
+  const token = await getUserToken();
+
+  if (!token) {
+    throw new Error("User not authenticated");
+  }
+
+  try {
+    const response = await fetch(`HTTP://${IP}/api/v1/hikes`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer: ${token}`,
+      },
+      body: JSON.stringify({
+        ...request,
+        coordinates: JSON.stringify(request.coordinates),
+      }),
+    });
+
+    if (!response.ok) {
+      throw new ApiError(`HTTP error: createHike: ${response.status}`, response.status);
+    }
+
+    return { success: true };
+  } catch (error) {
+    console.log("Fel vid skapandet av promenad:", error);
+    throw error;
+  }
 }
 
-export async function getAllHikes() {
-  return null
+export async function getAllHikes(): Promise<Hike[]> {
+  try {
+    const response = await fetch(`http://${IP}/api/v1/hikes`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new ApiError(`HTTP error: getAllHikes: ${response.status}`, response.status);
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 }
 
-export async function getAllHikesByUserId() {
-  return null
+export async function getAllHikesByUserId(userIdentifier: string): Promise<Hike[]> {
+  try {
+    const response = await fetch(`http://${IP}/api/v1/hikes?createdBy=${userIdentifier}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new ApiError(`HTTP error: getAllHikes: ${response.status}`, response.status);
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 }
 
-export async function deleteHike() {
-  return null
+export async function getHikeByIdentifier(hikeIdentifier: string): Promise<Hike> {
+  try {
+    const response = await fetch(`http://${IP}/api/v1/hikes/${hikeIdentifier}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new ApiError(`HTTP error: getHikeByIOdentifier: ${response.status}`, response.status);
+    }
+
+    return await response.json();
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+}
+
+export async function deleteHike(hikeIdentifier: string): Promise<{ success: boolean }> {
+  const token = await getUserToken();
+
+  if (!token) {
+    throw new Error("User not authenticated");
+  }
+
+  try {
+    const response = await fetch(`http://${IP}/api/v1/hikes/${hikeIdentifier}`, {
+      method: "DELETE",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${token}`,
+      },
+    });
+
+    if (!response.ok) {
+      throw new ApiError(`HTTP error: deleteHike: ${response.status}`, response.status);
+    }
+
+    return { success: true };
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
 }
