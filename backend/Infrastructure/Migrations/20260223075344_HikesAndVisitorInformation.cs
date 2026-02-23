d23bed810946b7576cb21617e46c601d14799fb2
Martin B <martinjborjesson@gmail.com>
2026-02-23 09:05:55 +0100

    add and apply migration

---
+++ backend/Infrastructure/Migrations/20260223075344_HikesAndVisitorInformation.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class HikesAndVisitorInformation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "Illumination",
+                table: "VisitorInformations",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "IlluminationText",
+                table: "VisitorInformations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "MaintainedBy",
+                table: "VisitorInformations",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<bool>(
+                name: "WinterMaintenance",
+                table: "VisitorInformations",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.CreateTable(
+                name: "Hikes",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    HikeLength = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Duration = table.Column<int>(type: "int", nullable: false),
+                    Coordinates = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedBy = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Identifier = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    LastUpdatedAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Hikes", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Hikes");
+
+            migrationBuilder.DropColumn(
+                name: "Illumination",
+                table: "VisitorInformations");
+
+            migrationBuilder.DropColumn(
+                name: "IlluminationText",
+                table: "VisitorInformations");
+
+            migrationBuilder.DropColumn(
+                name: "MaintainedBy",
+                table: "VisitorInformations");
+
+            migrationBuilder.DropColumn(
+                name: "WinterMaintenance",
+                table: "VisitorInformations");
+        }
+    }
+}
