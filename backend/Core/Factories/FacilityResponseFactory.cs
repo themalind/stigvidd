d8c6fc66d49e16b12ffe50807ba1aee647e0fc02
Themalind <therese.m.lindberg@gmail.com>
2026-05-06 13:27:39 +0200

    add responsefactory for facility

---
+++ backend/Core/Factories/FacilityResponseFactory.cs
@@ -0,0 +1,31 @@
+﻿using Infrastructure.Data.Entities;
+using WebDataContracts.ResponseModels.Facility;
+
+namespace Core.Factories;
+
+public class FacilityResponseFactory
+{
+    public FacilityResponse Create(Facility facility)
+    {
+        return FacilityResponse.Create(
+            facility.Identifier,
+            facility.Name,
+            (int)facility.FacilityType,
+            facility.IsAccessible,
+            facility.Latitude,
+            facility.Longitude
+        );
+    }
+
+    public IReadOnlyCollection<FacilityResponse> Create(IReadOnlyCollection<Facility> facilities)
+    {
+        return facilities.Select(facility => FacilityResponse.Create(
+            facility.Identifier,
+            facility.Name,
+            (int)facility.FacilityType,
+            facility.IsAccessible,
+            facility.Latitude,
+            facility.Longitude
+        )).ToList();
+    }
+}
