dfcaa282daf3123263e84047eb3e15f14055445a
Themalind <therese.m.lindberg@gmail.com>
2026-05-06 13:27:57 +0200

    Add interfaces for service and repo

---
+++ backend/Core/Interfaces/Services/IFacilityService.cs
@@ -0,0 +1,12 @@
+﻿using WebDataContracts.ResponseModels.Facility;
+
+namespace Core.Interfaces.Services;
+
+public interface IFacilityService
+{
+    Task<Result<FacilityResponse>> CreateFacilityAsync(string name, int facilityType, bool IsAccessible, decimal longitude, decimal latitude, CancellationToken ctoken);
+    Task<Result<IReadOnlyCollection<FacilityResponse>>> GetAllAsync(CancellationToken ctoken);
+    Task<Result<FacilityResponse>> GetByIdentifierAsync(string identifier, CancellationToken ctoken);
+    Task<Result<FacilityResponse>> UpdateFacilityAsync(string facilityIdentifier, string? name, int? facilityType, bool? isAccessible, decimal? longitude, decimal? latitude, CancellationToken ctoken);
+    Task<Result> DeleteAsync(string facilityIdentifier, CancellationToken ctoken);
+}
