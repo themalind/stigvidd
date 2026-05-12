f53c3d8c95a1f3f811f81233bf9ce123610bbdcc
Themalind <therese.m.lindberg@gmail.com>
2026-05-12 14:49:31 +0200

    Add hikesharerepo and missing validators

---
+++ backend/Core/Validators/TrailObstacleUpdateRequestValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+using WebDataContracts.RequestModels.TrailObstacle;
+
+namespace Core.Validators
+{
+    public class TrailObstacleUpdateRequestValidator : AbstractValidator<TrailObstacleUpdateRequest>
+    {
+        public TrailObstacleUpdateRequestValidator()
+        {
+            RuleFor(trailObstacleRequest => trailObstacleRequest.Description)
+                 .NotEmpty()
+                 .WithMessage("Description can not be empty")
+                 .MinimumLength(15)
+                 .MaximumLength(500)
+                 .When(x => x.Description != null);
+            RuleFor(trailObstacleRequest => trailObstacleRequest.IssueType)
+                .NotEmpty()
+                .WithMessage("IssueType can not be empty")
+                .When(x => x.IssueType != null);
+        }
+    }
+}
---
+++ backend/Core/Validators/UpdateHikeRequestValidator.cs
@@ -0,0 +1,31 @@
+﻿using FluentValidation;
+using WebDataContracts.RequestModels.Hike;
+
+namespace Core.Validators;
+
+public class UpdateHikeRequestValidator : AbstractValidator<UpdateHikeRequest>
+{
+    public UpdateHikeRequestValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty()
+            .MaximumLength(60)
+            .WithMessage("Name cannot be empty.")
+            .When(x => x.Name != null);
+        RuleFor(x => x.ParkingInfo)
+            .NotEmpty()
+            .MaximumLength(200)
+            .WithMessage("ParkingInfo cannot be empty.")
+            .When(x => x.ParkingInfo != null);
+        RuleFor(x => x.GettingThere)
+            .NotEmpty()
+            .MaximumLength(200)
+            .WithMessage("GettingThere cannot be empty.")
+            .When(x => x.GettingThere != null);
+        RuleFor(x => x.Description)
+            .NotEmpty()
+            .MaximumLength(500)
+            .WithMessage("Description cannot be empty.")
+            .When(x => x.Description != null);
+    }
+}
