4d93728224031fc12e4ee29687f185c22a68f412
Themalind <therese.m.lindberg@gmail.com>
2026-05-26 09:09:41 +0200

    Split responses to only initially fetch cords, and onpress fetch metadata

--- backend/WebDataContracts/ResponseModels/Trail/TrailPathResponse.cs
+++ backend/WebDataContracts/ResponseModels/Trail/TrailPathResponse.cs
@@ -3,26 +3,13 @@ namespace WebDataContracts.ResponseModels.Trail;
 public class TrailPathResponse
 {
     public required string Identifier { get; set; }
-    public required string Name { get; set; }
-    public bool IsAccessible { get; set; }
-    public decimal TrailLength { get; set; }
-    public int Classification { get; set; }
     public IReadOnlyCollection<LatLngPoint> Path { get; set; } = [];
 
-    public static TrailPathResponse Create(
-        string identifier,
-        string name, bool isAccessible,
-        decimal trailLength,
-        int classification,
-        IReadOnlyCollection<LatLngPoint> path)
+    public static TrailPathResponse Create(string identifier, IReadOnlyCollection<LatLngPoint> path)
     {
         return new TrailPathResponse
         {
             Identifier = identifier,
-            Name = name,
-            IsAccessible = isAccessible,
-            TrailLength = trailLength,
-            Classification = classification,
             Path = path
         };
     }
