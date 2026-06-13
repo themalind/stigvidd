1a1ed4885e5f85e28e5737c809ec1be263b27ec1
Themalind <therese.m.lindberg@gmail.com>
2026-06-13 16:07:52 +0200

    Add IUserPushTokenRepository

---
+++ backend/Core/Interfaces/Repositories/IUserPushTokenRepository.cs
@@ -0,0 +1,11 @@
+﻿using Infrastructure.Data.Entities;
+
+namespace Core.Interfaces.Repositories;
+
+public interface IUserPushTokenRepository
+{
+    Task<RepositoryResult> UpsertAsync(int userId, string expoToken, string platform, CancellationToken ctoken);
+    Task<RepositoryResult<IEnumerable<UserPushToken>>> GetTokensForUserAsync(int userId, CancellationToken ctoken);
+    Task<RepositoryResult<UserPushToken?>> GetByTokenAndUserAsync(string expoToken, int userId, CancellationToken ctoken);
+    Task<RepositoryResult> DeleteByTokenAsync(string expoToken, CancellationToken ctoken);
+}
