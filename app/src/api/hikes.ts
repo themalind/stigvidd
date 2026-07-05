c73074bc703116396eb514e3ab24d04250202752
Themalind <therese.m.lindberg@gmail.com>
2026-07-05 20:33:54 +0200

    Add unit tests for addTrail function and improve error handling in trails and hikes APIs

--- app/src/api/hikes.ts
+++ app/src/api/hikes.ts
@@ -23,8 +23,6 @@ export async function createHike(request: CreateHikeRequest): Promise<{ success:
       }),
     });
 
-    console.log(response.body);
-
     if (!response.ok) {
       throw new ApiError(`HTTP error: createHike: ${response.status}`, response.status);
     }
@@ -64,26 +62,6 @@ export async function updateHike(request: UpdateHikeRequest): Promise<Hike> {
   }
 }
 
-export async function getAllHikes(): Promise<Hike[]> {
-  try {
-    const response = await fetch(`${BASE_URL}/hikes`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    if (!response.ok) {
-      throw new ApiError(`HTTP error: getAllHikes: ${response.status}`, response.status);
-    }
-
-    return await response.json();
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
-}
-
 export async function getAllHikesByUserId(userIdentifier: string): Promise<Hike[]> {
   const token = await getUserToken();
 
@@ -101,26 +79,6 @@ export async function getAllHikesByUserId(userIdentifier: string): Promise<Hike[
 
     if (!response.ok) {
       throw new ApiError(`HTTP error: getAllHikesByUserId: ${response.status}`, response.status);
-    }
-
-    return await response.json();
-  } catch (error) {
-    console.log(error);
-    throw error;
-  }
-}
-
-export async function getHikeByIdentifier(hikeIdentifier: string): Promise<Hike> {
-  try {
-    const response = await fetch(`${BASE_URL}/hikes/${hikeIdentifier}`, {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-      },
-    });
-
-    if (!response.ok) {
-      throw new ApiError(`HTTP error: getHikeByIOdentifier: ${response.status}`, response.status);
     }
 
     return await response.json();
