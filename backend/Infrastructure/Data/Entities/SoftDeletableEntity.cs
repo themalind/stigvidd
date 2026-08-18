566c7fd1d813d3f214776dc3c6e034108bd4c988
Themalind <therese.m.lindberg@gmail.com>
2026-08-18 19:42:49 +0200

    remove SoftDeletableEntity class to simplify entity structure

--- backend/Infrastructure/Data/Entities/SoftDeletableEntity.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Infrastructure.Data.Entities;
-
-public abstract class SoftDeletableEntity : BaseEntity
-{
-    public bool IsDeleted { get; set; } = false;
-    public DateTime? DeletedAt { get; set; }
-}
